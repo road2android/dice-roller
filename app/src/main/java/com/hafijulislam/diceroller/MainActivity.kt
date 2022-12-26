3683dbc182a2e5277020bc0db962935885d4d37d
Hafijul Islam <hafijul233@gmail.com>
2022-12-27 02:02:46 +0600

    Code Refactored

--- app/src/main/java/com/hafijulislam/diceroller/MainActivity.kt
+++ app/src/main/java/com/hafijulislam/diceroller/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.hafijulislam.diceroller
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+
+class Dice(private val sides: Int = 6) {
+
+    fun roll(): Int {
+        return (1..this.sides).random()
+
+    }
+}
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ShowToast")
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+
+        rollButton.setOnClickListener {
+
+            val dice: Dice = Dice()
+
+            val randomNumber: Int = dice.roll()
+
+            if (randomNumber == 3) {
+                val toast = Toast.makeText(
+                    this,
+                    "It's your Lucky number \uD83E\uDD73",
+                    Toast.LENGTH_SHORT
+                )
+                toast.show()
+            }
+
+            this.updateImage(randomNumber)
+        }
+    }
+
+    private fun updateImage(number: Int) {
+
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        diceImage.contentDescription = number.toString()
+
+        (when (number) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        })
     }
 }
